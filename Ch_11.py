6fdae2a3eb4e0a75ba348ea5e962f9573cd9c445
Mikezp13 <zapantismichael1@gmail.com>
2025-09-17 23:02:14 +0300

    Update Ch_11.py

--- Ch_11.py
+++ Ch_11.py
@@ -1,29 +1,3 @@
-# import re
-#library regular expressions
-# hand=open('mbox.txt')
-
-# for line in hand:
-#     line=line.rstrip()
-#     if re.search('From:',line):
-#         print(line)
-
-# import re
-# x='My 2 favorite numbers are 19 and 42'
-# y=re.findall('[0-9+]+',x)
-# print(y)
-#
-# y=re.findall('[AEIOU]+',x)
-# print(y)
-#
-# import re
-# x='From: Using the : character'
-# y=re.findall('^F.+:',x)
-# print(y)
-
-# import re
-# x='From: Using the : character'
-# y=re.findall('^F.+?:',x)
-# print(y)
 
 import re
 hand=open('mbox.txt')
@@ -35,4 +9,5 @@ for line in hand:
     num=float(stuff[0])
     numlist.append(num)
 print('Maximum: ',max(numlist))
+
 
